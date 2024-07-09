e3471e46297728bd498663245f3ea08d0b5c7ee7
Bennet-Ukoh <bennetukoh@gmail.com>
2024-07-09 12:19:06 +0100

    mobile view updated

--- components/cart.tsx
+++ components/cart.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Cart() {
   return (
-    <section className="mx-4 my-8 sm:mx-10">
+    <section className="mx-4 my-8">
       <div className="text-sm text-gray-600">
         Home <span className="font-bold text-black">&gt;</span> Cart
       </div>
@@ -11,7 +11,7 @@ export default function Cart() {
       <div className="mt-6 flex flex-col space-y-6 sm:flex-row sm:items-start sm:justify-between sm:space-y-0">
         <div className="w-full space-y-6 border border-gray-300 p-4 sm:w-2/3">
           <div className="flex flex-col space-y-6 border-b border-gray-300 pb-6 sm:flex-row sm:justify-between sm:space-y-0">
-            <div className="flex space-x-4">
+            <div className="flex items-center space-x-4">
               <Image
                 src="/images/product1.png"
                 alt="product1"
@@ -19,15 +19,15 @@ export default function Cart() {
                 height={134.7}
                 className="rounded-[8px]"
               />
-              <div className="flex flex-col justify-between space-y-4">
+              <div className="flex flex-col justify-between">
                 <div>
                   <p className="text-lg font-bold">Men Cartoon Face Print</p>
-                  <p className="text-gray-500">Size: XXL</p>
+                  <p className="text-sm text-gray-500">Size: XXL</p>
                 </div>
                 <p className="text-xl font-bold">₦12,000</p>
               </div>
             </div>
-            <div className="flex flex-col items-center justify-between sm:space-y-10">
+            <div className="flex items-center justify-between sm:mt-4 sm:justify-start">
               <Image
                 src="/images/delete.png"
                 alt="delete icon"
@@ -35,15 +35,15 @@ export default function Cart() {
                 height={20}
                 className="cursor-pointer"
               />
-              <div className="flex items-center space-x-4 rounded-2xl bg-gray-200 px-6 py-2 text-2xl">
-                <button className="">-</button>
+              <div className="flex items-center space-x-2 rounded-full bg-gray-200 px-3 py-1 text-sm sm:px-6 sm:py-2 sm:text-2xl">
+                <button className="focus:outline-none">-</button>
                 <span>1</span>
-                <button className="">+</button>
+                <button className="focus:outline-none">+</button>
               </div>
             </div>
           </div>
           <div className="flex flex-col space-y-6 sm:flex-row sm:justify-between sm:space-y-0">
-            <div className="flex space-x-4">
+            <div className="flex items-center space-x-4">
               <Image
                 src="/images/product2.png"
                 alt="product2"
@@ -51,17 +51,17 @@ export default function Cart() {
                 height={134.7}
                 className="rounded-[8px]"
               />
-              <div className="flex flex-col justify-between space-y-4">
+              <div className="flex flex-col justify-between">
                 <div>
                   <p className="text-lg font-bold">
                     Alberta Sunset Men&apos;s Shirt
                   </p>
-                  <p className="text-gray-500">Size: XXL</p>
+                  <p className="text-sm text-gray-500">Size: XXL</p>
                 </div>
                 <p className="text-xl font-bold">₦12,000</p>
               </div>
             </div>
-            <div className="flex flex-col items-center justify-between sm:space-y-10">
+            <div className="flex items-center justify-between sm:mt-4 sm:justify-start">
               <Image
                 src="/images/delete.png"
                 alt="delete icon"
@@ -69,10 +69,10 @@ export default function Cart() {
                 height={20}
                 className="cursor-pointer"
               />
-              <div className="flex items-center space-x-4 rounded-2xl bg-gray-200 px-6 py-2 text-2xl">
-                <button className="">-</button>
+              <div className="flex items-center space-x-2 rounded-full bg-gray-200 px-3 py-1 text-sm sm:px-6 sm:py-2 sm:text-2xl">
+                <button className="focus:outline-none">-</button>
                 <span>1</span>
-                <button className="">+</button>
+                <button className="focus:outline-none">+</button>
               </div>
             </div>
           </div>
@@ -92,8 +92,8 @@ export default function Cart() {
               <p>Total</p>
               <p className="font-bold">₦27,000</p>
             </div>
-            <div className="flex items-center justify-between space-x-4">
-              <button className="flex items-center space-x-2 rounded-md bg-gray-200 px-8 py-4 text-black">
+            <div className="flex items-center space-x-4">
+              <button className="flex items-center space-x-2 rounded-md bg-gray-200 px-3 py-2 text-black">
                 <Image
                   src="/images/promocode.png"
                   alt="favorite icon"
@@ -102,11 +102,11 @@ export default function Cart() {
                 />
                 <span>Add a promo code</span>
               </button>
-              <button className="rounded-md bg-black px-8 py-4 text-white">
+              <button className="rounded-md bg-black px-3 py-2 text-white">
                 Apply
               </button>
             </div>
-            <button className="rounded-md bg-black px-8 py-4 text-white">
+            <button className="rounded-md bg-black px-8 py-2 text-white">
               Go to checkout
             </button>
           </div>
